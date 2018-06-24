5de314de345a1cb510e67a2ceeb12196fe6e1b5a
David Sanchez <david85trabajo@gmail.com>
2018-06-24 18:15:24 +0200

    headers

--- src/server.js
+++ src/server.js
@@ -24,6 +24,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
   }));
+  // Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.set('port', process.env.PORT || 3000);
 
@@ -32,13 +47,6 @@ app.use('/api/v1/', router);
 app.use('/api/v1/players', routerPlayers);
 app.use('/api/v1/teams', routerTeams);
 
-// Add headers
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  next();
-});
 
 app.listen(app.get('port'));
