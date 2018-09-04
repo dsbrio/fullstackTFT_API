44772cc3fc14acfa18aef9125a6856e2d4f5e469
cparandojo <carlosperezarandojo@gmail.com>
2018-09-04 18:38:47 +0200

    Encriptado de pass

--- src/utileria/general.js
+++ src/utileria/general.js
@@ -1,19 +1,13 @@
 
 //This function will return you the date and time in the following format: YYYY:MM:DD:HH:MM:SS. It also works in Node.js.
+//var crypto = require("crypto");
+var path = require("path");
+var fs = require("file-system");
+const NodeRSA = require('node-rsa');
 
 exports.getDateTime = () => {
 
     var date = new Date();
-
-    /*var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-*/
     var year = date.getFullYear();
 
     var month = date.getMonth() + 1;
@@ -25,3 +19,26 @@ exports.getDateTime = () => {
     return year + "-" + month + "-" + day; //+ ":" + hour + ":" + min + ":" + sec;
 
 };
+
+
+exports.decrypt = (encryptedPass, callback) => {
+
+    //obtenemos la clave privada
+    var rutaAbsolutaClavePrivada = path.resolve("./src/utileria/clavePrivada.key");
+
+    //tenemos la clave en base64
+    var clavePrivadaBase64 = fs.readFileSync(rutaAbsolutaClavePrivada, "utf8");
+   
+    //pasamos a byte la pass que nos llega
+    var bufferUsesPass = new Buffer(encryptedPass, "base64");
+
+    var  key = new NodeRSA(clavePrivadaBase64); //.decrypt(value, 'utf8');
+    key.setOptions({encryptionScheme: 'pkcs1'});
+    var decript = key.decrypt(bufferUsesPass,'utf8');
+    
+   console.log('decript',decript);
+
+    callback(decript);
+};
+
+
